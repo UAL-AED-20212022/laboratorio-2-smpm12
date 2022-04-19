4dddb6e337052218d82de681f0939cb4860e709b
Sérgio Melo <30008237@students.ual.pt>
2022-04-19 21:01:59 +0100

    lab2.0

---
+++ program.py
@@ -0,0 +1,13 @@
+from views import view as v
+import sys
+
+
+sys.stdout.reconfigure(encoding = 'UTF-8')
+if __name__ == "__main__":
+    v.main()
+
+
+#registo, consulta e eliminação de países com recurso a LinkedList
+
+
+
